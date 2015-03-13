3455dd51dcdf28546821403037f0d49bb1cdec06
bengruber <blgruber@gmail.com>
2015-03-13 17:53:31 -0500

    firsty first first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Huntawesome::Application.config.secret_key_base = '46585d736314cd3a978b7b5b2d4a8bee258af2a423100d6c6d322a9e61ce55063405c5759d56e317abd7411817d2e4a2aa7c2a5da61f7f40e366ed0bfbb36dcc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Huntawesome::Application.config.session_store :cookie_store, key: '_huntawesome_session'
