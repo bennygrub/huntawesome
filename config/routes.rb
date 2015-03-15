fa8d897cd0923c29507cad7f650974d3dde5f525
bengruber <blgruber@gmail.com>
2015-03-15 13:33:02 -0500

    adding devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Huntawesome::Application.routes.draw do
+  devise_for :users
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"
+  get "pages/faq"
+  root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
