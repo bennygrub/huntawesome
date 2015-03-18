b32e25862dba73788ab0960097d7d1d9cf868d65
bengruber <blgruber@gmail.com>
2015-03-17 23:24:39 -0500

    status styling started

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,5 +15,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @group = @user.groups
     @tasks = Task.where("level = ?", 1)
+    @completed_task_ids = @user.completed_tasks.map{|t| t.group_id}
+    @level_1_total_points = Task.sum(:points, conditions: {id: @completed_task_ids, level: 1})
   end
 end
