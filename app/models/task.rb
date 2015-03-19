f392e1708867ac515ed23208082a15f9b91653f7
bengruber <blgruber@gmail.com>
2015-03-19 09:25:22 -0500

    ASC order of tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ActiveRecord::Base
+  default_scope order('created_at ASC')
   has_many :completed_tasks
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/task-missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
